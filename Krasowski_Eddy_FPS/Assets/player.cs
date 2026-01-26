6eed251a79ab9a417fbe0ecc67fa83ef1eae4e10
Krasowskieddy <e.krasowski@bdx.ecolescreatives.com>
2026-01-26 18:01:10 +0100

    animation

--- Krasowski_Eddy_FPS/Assets/player.cs
+++ Krasowski_Eddy_FPS/Assets/player.cs
@@ -5,6 +5,8 @@ public class player : MonoBehaviour
     public float speed = 40;
     public float jump = 10;
     public float mouseX = 10;
+
+    public Animator myAnimator;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,28 +17,38 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(Vector3.up * jump * Time.deltaTime);
-        }
-        float xRotation = Input.GetAxis("Mouse X") * mouseX * Time.deltaTime;
-        
-        transform.Rotate(0, xRotation, 0);
+
+        myAnimator.SetBool("isMoving", false);
+        myAnimator.SetBool("isJumping", false);
+
+             if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                myAnimator.SetBool("isMoving", true);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.back * speed * Time.deltaTime);
+                myAnimator.SetBool("isMoving", true);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+                myAnimator.SetBool("isMoving", true);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+                myAnimator.SetBool("isMoving", true);
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(Vector3.up * jump * Time.deltaTime);
+                myAnimator.SetBool("isJumping", true);
+
+             }
+            float xRotation = Input.GetAxis("Mouse X") * mouseX * Time.deltaTime;
+
+            transform.Rotate(0, xRotation, 0);
     }
     }
